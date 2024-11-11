554dda39950cbc708759825537e39b0a8a0b0e7e
GUMMADI MANASA <154116785+manasamummy@users.noreply.github.com>
2024-11-11 16:18:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hellow world!!</h1>
+     <h2>My first basic application using ReactJs</h2>
+
+    </div>
+  );
+}
+
+export default App;
